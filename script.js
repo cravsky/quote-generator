dc3abdef4bc532af41b898295d3d346ca75b5de4
cravsky <cravsky@gmail.com>
2024-06-26 14:03:52 +0200

    Added loader

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
@@ -10,31 +11,42 @@ let apiQuotes = [];
 // Alternative source of quotes
 // https://zenquotes.io/api/random
 
-
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
-        const quote = getRandomQuote(apiQuotes);
-        authorText.textContent = quote.author ?? 'Unknown'; // .textContent sets the text content of an element
-
-        // Check quote length to determine styling
-        if (quote.text.length > 120) {
-            quoteText.classList.add('long-quote'); // .classlist.add() adds a class to an element
-        } else {
-            quoteText.classList.remove('long-quote'); // .classlist.remove() removes a class from an element
-        }
-
-        quoteText.textContent = quote.text;
+        showQuote();
     } catch (error) {
         alert(error)
     }
 }
 
+function showQuote() {
+    loading();
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    authorText.textContent = quote.author ?? 'Unknown'; // .textContent sets the text content of an element
+    // Check quote length to determine styling
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote'); // .classlist.add() adds a class to an element
+    } else {
+        quoteText.classList.remove('long-quote'); // .classlist.remove() removes a class from an element
+    }
+    quoteText.textContent = quote.text;
+    complete()
+}
 
-function getRandomQuote(quotes) {
-    return quotes[Math.floor(Math.random() * quotes.length)];
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
 }
 
 // Tweet Quote
@@ -44,9 +56,8 @@ function tweetQuote() {
 }
 
 // Event Listeners
-newQuoteBtn.addEventListener('click', getQuotes);
+newQuoteBtn.addEventListener('click', showQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuotes();
-// console.log(getRandomQuote(apiQuotes));
